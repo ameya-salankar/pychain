9b704ea2c6a950308e04272dbdc4c57117066d33
Ameya Salankar <salankaras1234@gmail.com>
2020-04-03 11:17:50 +0530

    documented

--- client.py
+++ client.py
@@ -8,30 +8,28 @@ from pprint import pprint
 
 CONNECTED_NODE_ADDRESS = "http://127.0.0.1:8001"
 
+# Initialize the client
 client = Client()
 model = (client.model_build()).get_weights()
 models = np.array([0, 0])
-print(len(model))
 
 
 for i in range(10):
-
+    # Get the recent most block from the chain, which contains the avg weights
     response = requests.get("{}/recent_block".format(CONNECTED_NODE_ADDRESS))
-    # content = []
+    
+    # The first iteration won't have any avg weights from server
     if i != 0 and response.status_code == 200:
         model = []
         content = json.loads(response.content)
         modeld = json.loads(content["block"][0]["content"])
         for i in modeld:
             model.append(np.array(i))
-        # print(content, "\n", model)
 
-    # pprint(model)
-    # pprint(content, "\n", model)
-
-    model_list = []
-    # model = np.array(model)
     models = client.Federated_model(i, model)
+
+    # Convert the model into list
+    model_list = []
     for model in models:
         z = []
         for j in model:
@@ -43,7 +41,6 @@ for i in range(10):
         "content": json.dumps(model_list),
     }
 
-    # pprint(post_object, "\n", models)
     # Submit a transaction
     new_tx_address = "{}/new_transaction".format(CONNECTED_NODE_ADDRESS)
 
@@ -51,4 +48,5 @@ for i in range(10):
         new_tx_address, json=post_object, headers={"Content-type": "application/json"},
     )
 
+    # Then immediately mine
     requests.get("{}/mine".format(CONNECTED_NODE_ADDRESS))
